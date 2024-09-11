00453fcd39c6448dce30043cf626f66555991ff8
nwt5144 <nwt5144@psu.edu>
2024-09-11 13:05:53 -0400

    Update write_ids.c

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "nwt5144";
+    char STUDENT_ID[] = "947943162";
 
     return 0 ;
 }
